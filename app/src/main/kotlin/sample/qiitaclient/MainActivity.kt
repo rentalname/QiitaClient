6f6e5857876c17af3bed4988d655e668a97c52b0
rentalname <agenda21st@gmail.com>
2016-10-20 15:41:03 +0900

    記事リスト表示 表示機能部分を実装
    ダミーで設定した記事データ配列に対して, リスト表示を可能とした

--- app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
@@ -1,23 +1,30 @@
 package sample.qiitaclient
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.model.User
-import sample.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val articleView = ArticleView(applicationContext)
-        articleView.setArticle(
-                Article(id = "123",
-                        title = "Kotlin入門",
-                        url = "https://http.cat/200",
-                        user = User(id = "456",
-                                    name = "ashina",
-                                    profileImageUrl = "")))
-        setContentView(articleView)
+        setContentView(R.layout.activity_main)
+
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(
+                dummyArticle("Kotlin入門", "ashina"),
+                dummyArticle("Java入門", "ishida")
+        )
+
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        listView.adapter = listAdapter
     }
+
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
 }
